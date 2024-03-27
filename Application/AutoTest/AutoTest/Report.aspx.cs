b350cc100995824b5d9cf0b79fb09efa60372560
Mahsen <not.select@gmail.com>
2024-03-27 13:26:22 +0330

    Add report file to export report serial from link

---
+++ Application/AutoTest/AutoTest/Report.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AutoTest
+{
+    public partial class Report : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String GetReport = Request.Params["GetReport"];
+            if (GetReport != null)
+            {
+                Response.Write(Default.CheckSerial(GetReport));
+                Context.ApplicationInstance.CompleteRequest();
+            }
+        }
+    }
+}
